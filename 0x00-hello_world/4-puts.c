8ab961d84cc56bdc864b152488d5d5847ae729f8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-16 16:41:43 -0700

    script 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  */
 int main(void)
 	{
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	/**
 	 * here is a return to end with 0
 	 */
+
 	return (0);
 	}
