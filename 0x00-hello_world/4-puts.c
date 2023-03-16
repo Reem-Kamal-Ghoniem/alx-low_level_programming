81406fd295f57b6df533c81efcc16270c1cbc870
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-16 16:38:34 -0700

    script 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * here is the main function
+ */
+int main(void)
+	{
+	puts("/"Programming is like building a multilingual puzzle");
+	/**
+	 * here is a return to end with 0
+	 */
+	return (0);
+	}
