a3fa924c3b2eca922f93326801e1dfeea7cc6124
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-21 16:33:17 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@
 
 void print_alphabet_x10(void)
 {
-        char i;
+	char i;
 	int j = 0;
 
 	for (j = 0; j <10; j++)
 	{
-        	for (i = 'a'; i <= 'z'; i++)
-        	{
-                	_putchar(i);
-        	}
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
 		_putchar('\n');
 	}
 }
