71cb0ef0a04224ac7036377314ac4de84c191c37
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-22 15:00:27 -0700

    debug 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: void
+*/
+
+void main(void)
+{
+        if (positive_or_negative(0) == "0 is zero")	
+		printf ("0 is zero");
+}
