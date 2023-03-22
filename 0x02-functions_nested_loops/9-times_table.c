4c575cbbf644e553b2c49af86bdc12c16b24d838
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-21 17:48:07 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* times_table - a function that checks for lowercase character.
+*
+* Return: void.
+**/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar((i * j) + '0');
+			if (j != 9)
+				_putchar(',');
+		}
+		_putchar(\n);
+	}
+	return (0);
+}
