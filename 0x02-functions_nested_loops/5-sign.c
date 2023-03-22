72f83602490bde5f5c9547c8af36331349246735
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-21 17:01:32 -0700

    task 55

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _islower - a function that checks for lowercase character.
+*
+* @n: the input to be checked
+*
+* Return: depend on n
+*/
+
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 1)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0)
+	}
+}
