c6f3351972cd0f0d17703d9dc431f642063668b5
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-21 17:24:15 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_last_digit - a function that prints the last digit
+*
+* @num: the input to be checked
+*
+* Return: last digit.
+*/
+
+int print_last_digit(int num)
+{
+	int mod;
+
+	mod = num % 10;
+	return (mod);
+}
