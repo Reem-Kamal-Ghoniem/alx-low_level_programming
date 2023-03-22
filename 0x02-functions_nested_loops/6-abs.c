eaed029caac9330727ff44a482558ce52607628b
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-21 17:15:40 -0700

    task 5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _abs - perform absolute
+*
+* @c: the input to be checked
+*
+* Return: abolute
+*/
+int _abs(int c)
+{
+	if (c > 0 || c = 0)
+		return (n);
+	else
+		return (0-n);
+}
