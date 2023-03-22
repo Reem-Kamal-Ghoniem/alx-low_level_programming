6e958e2b5a01735791f2a5bbcc617f1cec3dd181
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-21 18:10:49 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,12 +16,14 @@ void times_table(void)
 			prod = i * j;
 			if (prod > 9)
 				_putchar((prod / 10) + '0');
-			else if (prod < 9 && prod != 0)
+			else if (prod <= 9 && prod != 0)
 				_putchar(' ');
 			_putchar((prod % 10) + '0');
 			if (j != 9)
+			{
 				_putchar(',');
-			_putchar(' ');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
