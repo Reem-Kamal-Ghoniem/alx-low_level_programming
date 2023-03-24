c7b1f7add875d922e469f8e3c83333ad33b27ef3
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-23 18:45:32 -0700

    task 1, 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isdigit -  a function that checks for a digit 0-9
+ * @a: input 1
+ * @b: input 2
+ * Return: the result of thier mult
+ */
+int mul(int a, int b)
+{
+	return(a*b);
+}
