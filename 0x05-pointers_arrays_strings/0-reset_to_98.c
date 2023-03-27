4250a2144b9aad71f779561e67bfe884e3ce3d93
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-27 15:48:23 -0700

    pointers 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/* reset_to_98 - a function that takes a pointer ant make it's pointed value to zero
+ * *n: a pointer to be changed
+ * returns: void
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+	int var;
+
+	n = &var;
+	*n = 98;
+}
