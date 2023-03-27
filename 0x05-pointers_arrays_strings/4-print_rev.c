98d9c1ded837d100a10a2bb7b13d730c0288d28b
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-27 16:36:08 -0700

    pointers 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts -  a function that prints a string, in reverse, followed by a new line.
+ * @s: pointer to a string string
+ * returns - void
+ */
+void print_rev(char *s);
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (count = count -  1; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar ('\n');
+}
