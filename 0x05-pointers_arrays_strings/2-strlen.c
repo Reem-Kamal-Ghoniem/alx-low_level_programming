119965868efa1f9d16246de5131f98e3c48f0eb6
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-03-27 16:10:38 -0700

    pointers 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ * @s: the input string
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen (s));
+}
