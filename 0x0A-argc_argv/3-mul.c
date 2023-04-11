b563da20c6eecfecbf9322031f97f9ddbe217e37
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-10 17:43:15 -0700

    arg_multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints multiplication of arguments it receives.
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: int
+ **/
+int main(int argc, char **argv)
+{
+	int mul = 0;
+
+	if (argc < 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+		return (0);
+	}
+}
