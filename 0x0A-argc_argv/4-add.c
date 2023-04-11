3df164c7bd23559c7eda8bfeb74f09a5e2adc51f
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-10 18:41:51 -0700

    arg_addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * main - prints addition of arguments it receives.
+ * @argc: number of arguments
+ * @argv: array of strings
+ * Return: int
+ **/
+int main(int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 0; i <= argc; i++)
+	{
+		if (!(isdigit(atoi(argv[i]))))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+			printf("%d", sum);
+			return (0);
+		}
+	}
+}
