4e129b300e22a043a78c04af287bad7f9125d517
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-11 12:31:25 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	ptr = (char *)malloc(sizeof(char) * size);
-
+	if (ptr NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
