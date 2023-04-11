df1239ee68176bb66a9f631dc2199ce5e355f3d2
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-10 19:55:46 -0700

    arg_addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	for (i = 1; i < argc; i++)
-	{	
+	{
 		for (j = 0; argv[j][j] != '\0'; j++)
 		{
 			if (!(isdigit(argv[i][j])))
@@ -28,8 +28,7 @@ int main(int argc, char **argv)
 				printf("Error\n");
 				return (1);
 			}
-		}	
-		
+		}
 			sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
