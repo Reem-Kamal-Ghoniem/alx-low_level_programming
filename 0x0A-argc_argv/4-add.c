2e45bd6c68108f476735873c72c05e6ed5e4cd48
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-10 19:30:05 -0700

    arg_addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 		printf("0\n");
 		return (0);
 	}
-	for (i = 0; i <= argc; i++)
+	for (i = 1; i <= argc; i++)
 	{	
 		for (j = 0; j <= (int)strlen(argv[i]); j++)
 		{
