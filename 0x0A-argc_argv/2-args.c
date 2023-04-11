946de9cb03b491f34a10c5eaa266454ff6b31699
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-10 17:18:37 -0700

    arg_names

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints all arguments it receives
+ * followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of strings of size argc
+ * Return: integer
+ **/
+int main(int argc, char **argv)
+{
+	int i;
+
+	for(i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
