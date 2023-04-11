a164aa0f2cde7b00a77bdddbd2386abe5c0c1a06
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-10 22:00:27 -0700

    change money

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,28 +13,30 @@
 int main(int argc, char **argv)
 {
 	int coins = 0;
+	int money = atoi(argv[1]);
 
-	if (argc !== 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (atoi(argv[1] < 0)
+	else if (atoi(argv[1]) < 0)
 		printf("0\n");
 	else
 	{
-		while (atoi(argv[1]) != 0)
+		while ( money != 0)
 		{
-		if (atoi(argv[1]) >= 25)
-			atoi(argv[1]) -= 25, coins++;
-		else if (atoi(argv[1]) >= 10 && atoi(argv[1]) < 25)
-			atoi(argv[1]) -= 10, coins++;
-		else if (atoi(argv[1]) >= 5 && atoi(argv[1]) < 10)
-			atoi(argv[1]) -= 5, coins++;
-		else if (atoi(argv[1]) >= 2 && atoi(argv[1]) < 5)
-			atoi(argv[1]) -= 2, coins++;
-		else (cents == 1)
-			atoi(argv[1]) -= 1, coins++;
+		if (money >= 25)
+			money = money - 25, coins++;
+		else if money >= 10 && money < 25)
+			money = money - 10, coins++;
+		else if (money >= 5 && money < 10)
+			money = money - 5, coins++;
+		else if (money >= 2 && money < 5)
+			money = money - 2, coins++;
+		else (money == 1)
+			money = money -  1, coins++;
 		}
 	}
+	return (0);
 }
