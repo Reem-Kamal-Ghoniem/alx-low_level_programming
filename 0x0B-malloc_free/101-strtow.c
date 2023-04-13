c4e1b407a17d1b689d4d6568dace811213eab971
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-12 19:36:04 -0700

    advanced malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,15 +11,16 @@ char **strtow(char *str)
 {	int i, count = 0;
 	char **words_count;
 
-	if (str == NULL || str == " ")
+	if (str == NULL || str[0] == 0)
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0')
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			count++;
 	}
 	words_count = (char **)malloc(sizeof(char *) * (count + 1));
 	if (words_count == NULL)
 		return (NULL);
+	return (woeds_count);
 }
