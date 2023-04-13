92bdf9b4d6b07f5d8b9194a1e6b0eb0bfb2a2056
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-13 15:49:21 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-		new_ptr == malloc(new_size);
+		new_ptr = malloc(new_size);
 		if (new_ptr == NULL)
 			return (NULL);
 		else
@@ -33,6 +33,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_ptr == NULL)
 		return (NULL);
 	for (i = 0; i <= old_size; i++)
-		new_ptr[i] = (char *)ptr[i];
+		new_ptr[i] = ((char *)ptr)[i];
 	return (new_ptr);
 }
