c9412375facd590bee9ae865c0ceabefa158bd58
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-13 11:16:04 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,11 +14,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
-		return NULL;
+		return (NULL);
 
 	ptr = (void *)malloc(nmemb * size);
 	if (ptr == NULL)
-		return NULL;
+		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		ptr[i] = 0;
 
