126c8ee49cdd1bd27aed0ccc2ce996ddf138a3d8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-13 19:08:39 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,18 +12,25 @@
 int main(int argc, char *argv[])
 {
 	int mul, num1, num2;
-	/*num1 = (int *)argv[1];*/
-	/**num2 = (int *)argv[2];*/
 
 	if (argc != 3 || isdigit(num1) || isdigit(num2))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
+	for (i = 0; i < argc; i++)
 	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
+		for (j = 0; (argv[i][j] != '\0'); j++)
+		{
+			if (!(isdigit(argv[i][j])))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		}
 	}
+		/*mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);*/
+	
 	return (0);
 }
