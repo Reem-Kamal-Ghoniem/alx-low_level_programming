d7b30cf3452662fc84f8079d47435d5a1a6dd3fd
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-17 13:15:58 -0700

    preprocessor 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main -  prints the name of the file it was compiled from
+ * return - 0
+ */
+
+int main(void)
+{
+	printf("%d\n", __FILE__);
+	return (0);
+}
