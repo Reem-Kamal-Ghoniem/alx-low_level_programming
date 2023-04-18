84e2b60732299088a9c08e766c4e16d44af15e8a
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-17 17:13:06 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - new client type
+ * @d: struct name
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
