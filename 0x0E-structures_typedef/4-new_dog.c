7854c0d15d6f1e73760cb8825c76c01302aa6e7d
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-17 18:13:20 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - new client type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
