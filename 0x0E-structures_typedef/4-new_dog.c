d5449f2951415a4b2ed45bc48cdcaddcadddb235
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-17 19:36:42 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
+#include <string.h>
 /**
  * new_dog - new client type
  * @name: dog name
@@ -9,7 +10,29 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *poppy;
+	dog_t * poppy;
+	int namelen, ownlen;
+	int i;
+
+	namelen = strlen(name);
+	ownlen = strlen(owner);
+
+	poppy = malloc(sizeof(dog_t));
+	if (poppy == NULL)
+		return (NULL);
+
+	 poppy->name = malloc(sizeof(dog_t));
+        if (poppy == NULL)
+                return (NULL);
+	for (i = 0; i < namelen; i++)
+		(*poppy).name[i] = name[i];
+	(*poppy).age = age;
+
+	poppy->owner = malloc(sizeof(dog_t));
+	if (poppy == NULL)
+		return (NULL);
+	for (i = 0; i < ownlen; i++)
+		poppy->owner[i] = owner[i];
 
 	return (poppy);
 }
