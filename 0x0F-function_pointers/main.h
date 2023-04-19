98e0f01767fdbc1641868eaf6fcb8961260da8e8
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-19 01:18:17 -0700

    task 2

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,5 +1,5 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
