53cb8157ab6bddeb9d6410fabd9943fb5e050c3a
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-19 12:36:56 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,11 @@
  * print_name - a function that prints a name
  * @name: the name to be printed
  * @f: ppointer to that name
- * Return - void
+ *
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f = NULL)
+		return;
 	(*f)(name);
 }
