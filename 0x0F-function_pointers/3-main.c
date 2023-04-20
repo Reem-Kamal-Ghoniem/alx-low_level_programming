435a70e8d0dd1074130d5e56e28801b48902c194
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-19 19:18:02 -0700

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,20 +10,14 @@
 int main(int argc, char *argv[])
 {
 	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		printf("Error\n"), exit(98);
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	if (((*argv[2] == '/') || (*argv[2] == '%')) && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+		printf("Error\n"), exit(100);
 
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
