6756163901500de8678a5a32f70f6ce5937aafe1
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-19 18:52:35 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,26 +1,52 @@
 #include "3-calc.h"
 #include <stdio.h>
-/*
+/**
  *op_add - add a and b
- *op_sub - subtract a and b
- *op_mul - multiply a and b
- *op_div - divide a and b
- *op_mod - calc the remainder of the dividion of a and b
  *@a: first number
  *@b: second number
  *Return: int
  */
 int op_add(int a, int b)
+{
 	return (a + b);
-
+}
+/**
+ *op_sub - subtract a and b
+ *@a: first number
+ *@b: second number
+ *Return: int
+ */
 int op_sub(int a, int b)
-	return (a - b)
-
+{
+	return (a - b);
+}
+/**
+ *op_mul - multiply a and b
+ *@a: first number
+ *@b: second number
+ *Return: int
+ */
 int op_mul(int a, int b)
+{
 	return (a * b);
-
+}
+/**
+ *op_div - divide a by b
+ *@a: first number
+ *@b: second number
+ *Return: int
+ */
 int op_div(int a, int b)
+{	
 	return (a / b);
-
+}
+/**
+ *op_mod - remainder of a divided b
+ *@a: first number
+ *@b: second number
+ *Return: int
+ */
 int op_mod(int a, int b)
+{
 	return (a % b);
+}
