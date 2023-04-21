0c91b1a5e59a4d59e1e068006b9e0cd8f06d591d
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-20 18:48:31 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_all - function that prints all the types
+ * @format: arguments
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	if (format == null)
+		return;
+}
