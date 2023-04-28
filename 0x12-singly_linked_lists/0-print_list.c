182a69e47d0bddbffb81cca020e2adbe2c379fa3
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-27 18:39:43 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
  * print_list - prints a singly linked list
  * @h: pointer to the list
@@ -9,11 +9,11 @@ size_t print_list(const list_t *h)
 	int i;
 	for (i = 0; h != NULL; i++)
 	{
-		if (!(*h.str))
+		if (!(h->str))
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", *h.len, *h.str);
-		h = *h.next;
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
 	}
 	return (i);
 }
