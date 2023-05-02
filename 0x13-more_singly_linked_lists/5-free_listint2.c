407c79ffc15cbd2104de95e4af30d0ec3dc83937
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-02 06:45:18 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ *free_listint2 - a function that frees a list_t list
+ *@head: head pointer
+ *Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	lisintt_t *save;
+
+	while (*head)
+	{
+		save = *head->next;
+		free(*head->str);
+		free(*head);
+		*head = save;
+	}
+}
