cd350388a0a230c23dfc47af9582b65db72f1d0c
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-02 05:41:15 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints a singly linked list
+ * @h: pointer to the list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+	{
+		if (!(h->str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
