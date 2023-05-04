c6042b4a3e50983e48c0a1dbe5b7ee4a34398a2c
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-04 15:51:08 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,13 +9,14 @@ unsigned int binary_to_uint(const char *b)
 {
 	int index, i, base = 1;
 	unsigned int unit;
+
 	if (!b)
 		return (0);
 
 	for (index = 0; b[index]; index++)
 	{
-			if (b[index] != '0' && b[index] != '1')
-				return (0);
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
 	}
 	for (i = index; index; index--)
 	{
