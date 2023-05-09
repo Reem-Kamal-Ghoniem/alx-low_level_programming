3b39a46eddca24bd6f8b6647bda449d04cdb5027
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-08 19:30:04 -0700

    task 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,16 +13,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (text_content)
-	{
-		while (text_content[i])
-			i++;
-	}
 	file = open(filename, O_CREAT |O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	if (text_content)
-		file1 = write(file, text_content, i);
+		file1 = write(file, text_content, strlen(text_content));
 	if (file1 == -1)
 		return (-1);
 
