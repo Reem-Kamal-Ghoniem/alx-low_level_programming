123a3c7485dc5f26ef01974b1b014cfe2f117e44
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-05-08 19:10:15 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[])
 {
 	int from, to, read_from = 1, write_to;
-	char buf[BUFSIZ];
+	char buf[1024];
 	mode_t Permissions = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 
 	if (argc != 3)
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, Permissions);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 1024);
 	if (to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while (read_from)
