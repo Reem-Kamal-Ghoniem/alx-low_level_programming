4cae713c6bf1962db06937434580b5b9510bfc0f
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-06-23 13:58:45 -0700

    C to python

---
+++ 0x18-dynamic_libraries/CtoPY.c
@@ -0,0 +1,51 @@
+#include "CtoPY.h"
+/**
+ * add - add to numbers
+ * @a: first num
+ * @b: second num
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - add to numbers
+ * @a: first num
+ * @b: second num
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - add to numbers
+ * @a: first num
+ * @b: second num
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - add to numbers
+ * @a: first num
+ * @b: second num
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - add to numbers
+ * @a: first num
+ * @b: second num
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
