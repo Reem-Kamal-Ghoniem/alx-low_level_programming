278af8961219aafc5d45f9d8d180f56227f3de3b
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-07-06 19:33:51 -0700

    task 0.2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (table->array == NULL)
 		return (NULL);
-	for (int i = 0; i < table->size; i++)
+	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
 	return (table);
 }
